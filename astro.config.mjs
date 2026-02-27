99efdccafdb90eb39fc32eab64a38d712bba7625
homepagesya1 <yannick.salm@outlook.de>
2026-02-27 16:35:48 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://homepagesya1.github.io',
+  output: "server",
+  adapter: cloudflare(),
 });
