b2e34331320876c993bc09840cb97b201e33eae9
homepagesya1 <Yannick.salm@outlook.de>
2026-03-07 21:34:55 +0100

    creation site-map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://yannicksalm.ch',
+
   output: 'server',
+
   adapter: cloudflare(),
+
+  integrations: [
+    sitemap()
+  ]
 });
